2baeba867e5c7eb8cbe3e90a9257165f04255ad4
Aditya <adityafav@gmail.com>
2022-12-08 00:04:43 +0530

    setup done

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const MongoClient = require("mongodb").MongoClient;
+
+// Connection URL
+const url = process.env.MONGO_URL;
+const client = new MongoClient(url);
+
+const dbName = "live";
+client.connect();
+console.log("Connected successfully to server");
+const db = client.db(dbName);
+const areas = db.collection("Area");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async function (req, res, next) {
+  let query = await areas.find({}).toArray();
+  // console.log(query);
+  res.json(query);
+  // res.render("index", { title: "Express" });
 });
 
 module.exports = router;
